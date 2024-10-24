61d6b56d4b7004f5727e60b3918f0c18d73ae4f1
memnochdavid <davduquediaz@gmail.com>
2024-10-24 15:23:24 +0200

    haciendo ejercicios

---
+++ ejerciciosT3.py
@@ -0,0 +1,18 @@
+ej=int(input("Ejercicio a probar:"))
+match(ej):
+    case 1:
+        '''Ejercicio 1. Crea una función que obtenga el máximo de una lista de números'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+def x01(lista) ->
